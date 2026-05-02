a1a00244fa5acf2a69824d5775da019ed8563f93
waleedahmad7970 <waleedahmad7970@gmail.com>
2026-05-02 15:34:59 +0500

    login and signup pages

--- src/App.tsx
+++ src/App.tsx
@@ -46,6 +46,8 @@ const App = () => (
               <Routes>
                 <Route element={<SiteLayout />}>
                   <Route path="/" element={<Home />} />
+                   <Route path="/login" element={<Login />} />
+                  <Route path="/signup" element={<Signup />} />
                 
                 </Route>
 
